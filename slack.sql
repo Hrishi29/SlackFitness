080e18681a80337a082173a8b70bbc467089652a
Hrishi29 <31528997+Hrishi29@users.noreply.github.com>
2017-10-16 14:40:41 -0400

    modified database
    addition of foreign keys. Normalisation is being done.

--- slack.sql
+++ slack.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 03, 2017 at 05:59 PM
+-- Generation Time: Oct 16, 2017 at 06:39 PM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `slack`
 --
-CREATE DATABASE IF NOT EXISTS `slack` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `slack`;
 
 -- --------------------------------------------------------
 
@@ -28,26 +26,62 @@ USE `slack`;
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `Id` int(20) NOT NULL,
-  `workspace` varchar(100) NOT NULL,
-  `user_email` varchar(100) NOT NULL,
-  `user_pass` varchar(100) NOT NULL,
-  `user_name` varchar(100) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `id` int(20) NOT NULL,
+  `workspace` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`Id`, `workspace`, `user_email`, `user_pass`, `user_name`) VALUES
-(1, 'oducs518f17', 'mater@rsprings.gov', '@mater', 'Tow Mater'),
-(2, 'oducs518f17', 'porsche@rsprings.gov', '@sally', 'Sally Carrera'),
-(3, 'oducs518f17', 'hornet@rsprings.gov', '@doc', 'Doc Hudson'),
-(4, 'oducs518f17', 'topsecret@agent.org', '@mcmissile', 'Finn McMissile'),
-(5, 'oducs518f17', 'kachow@rusteze.com', '@mcqueen', 'Lightning McQueen'),
-(6, 'oducs518f17', 'chinga@cars.com', '@chick', 'Chick Hicks');
+INSERT INTO `users` (`id`, `workspace`) VALUES
+(1, 'oducs518f17');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_channel`
+--
+
+CREATE TABLE `users_channel` (
+  `id` int(20) NOT NULL DEFAULT '0',
+  `channels` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users_channel`
+--
+
+INSERT INTO `users_channel` (`id`, `channels`) VALUES
+(1, 'workouts'),
+(1, 'nutrition'),
+(1, 'crossfit');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_info`
+--
+
+CREATE TABLE `users_info` (
+  `id` int(20) NOT NULL DEFAULT '0',
+  `user_email` varchar(100) NOT NULL DEFAULT '',
+  `user_pass` varchar(100) NOT NULL,
+  `user_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users_info`
+--
+
+INSERT INTO `users_info` (`id`, `user_email`, `user_pass`, `user_name`) VALUES
+(1, 'mater@rsprings.gov', '@mater', 'Tom Mater'),
+(1, 'porsche@rsprings.gov', '@sally', 'Sally Carrera'),
+(1, 'hornet@rsprings.gov', '@doc', 'Doc Hudson'),
+(1, 'topsecret@agent.org', '@mcmissile', 'Finn McMissile'),
+(1, 'kachow@rusteze.com', '@mcqueen', 'Lightning McQueen'),
+(1, 'chinga@cars.com', '@chick', 'Chick Hicks');
 
 --
 -- Indexes for dumped tables
@@ -57,7 +91,19 @@ INSERT INTO `users` (`Id`, `workspace`, `user_email`, `user_pass`, `user_name`) 
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`Id`);
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users_channel`
+--
+ALTER TABLE `users_channel`
+  ADD KEY `id` (`id`);
+
+--
+-- Indexes for table `users_info`
+--
+ALTER TABLE `users_info`
+  ADD KEY `id` (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -67,7 +113,23 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `Id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `users_channel`
+--
+ALTER TABLE `users_channel`
+  ADD CONSTRAINT `users_channel_ibfk_1` FOREIGN KEY (`id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `users_info`
+--
+ALTER TABLE `users_info`
+  ADD CONSTRAINT `users_info_ibfk_1` FOREIGN KEY (`id`) REFERENCES `users` (`id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
