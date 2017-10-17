213418259b745fd66e84a4b01736987d42b1b525
Hrishi29 <31528997+Hrishi29@users.noreply.github.com>
2017-10-17 14:57:08 -0400

    done

--- index3.php
+++ index3.php
@@ -19,11 +19,16 @@ $conn = mysqli_connect("localhost","admin","M0n@rch$");
 	
 	mysqli_select_db($conn,'slack');
 	
-	if(isset($_POST['submit']))
-{
-    $message_post=$_POST['message_post'];//here getting result from the post array after submitting the form.
-	$user_name=$_SESSION['user_name'];
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	
+	
+	
+
+    $message_post=$_POST['message_post'];//here getting result from the post array after submitting the form.
+	$message_post = trim($message_post);
+	$message_post = stripslashes($message_post);
+	$message_post = htmlspecialchars($message_post);
+  
 	
 	$insert_channel=" INSERT INTO users_message (ch_id, messages, user_name, date) VALUES ('3', '$message_post', '$user_name', CURRENT_TIMESTAMP())"  ;
 
