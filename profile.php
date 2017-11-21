66d7f21ea98c2d7a8b6e68fef7eb44b9bc65640c
Hrishi29 <31528997+Hrishi29@users.noreply.github.com>
2017-11-21 08:54:11 -0500

    testing

--- profile.php
+++ profile.php
@@ -30,7 +30,6 @@ if(!isset($_SESSION['workspace'])){ //only users within workspace
 <div class="col-md-6 col-md-offset-3">
 <center>
 
-<h1 style="font-family: 'Salsa'">Profile Page</h2>
 
 <?php
 
@@ -50,6 +49,8 @@ if(isset($_POST['search_submit'])) {
 	
 	?>
 	
+	<h1 style="font-family: 'Salsa'"><?php echo $profile2['user_name'];?></h2>
+
 	
 	<img src="user_images/<?php echo $profile2['user_pic'];?>" class="img-rounded responsive"   width="200" height="200"> 
 
@@ -64,20 +65,59 @@ if(isset($_POST['search_submit'])) {
 	<p><h2 style="font-family: 'Salsa'">Email Id:</h2><span style="margin-left: 10px; font-family: 'Patua One'; font-size:2em; font-weight:bold"><?php echo $profile2['user_email'];?></span></p>	
 	<br>
 	
-	<a href="index.php" class="btn btn-primary btn-md" role="button">Back</a>
+	
+	<p><h2 style="font-family: 'Salsa'">Number of Posts:</h2><span style="font-family: 'Patua One'; font-size:2em; font-weight:bold"><?php 
+	
+	$insert_posts = mysqli_query($conn,"select  * from users_message where user_name='".$profile_mess."'");
+	
+	if($insert_posts){	
+											// Return the number of rows in result set
+							$rowcount=mysqli_num_rows($insert_posts);
+						    echo $rowcount;
+	
+	
+					  }
 
+			else{?>0<?php }?></span></p>	
+	<br>
+	
+	
+	<p><h2 style="font-family: 'Salsa'">Number of Positive Reactions:</h2><span style="font-family: 'Patua One'; font-size:2em; font-weight:bold"><?php 
+	
+	$insert_posts1 = mysqli_query($conn,"select  thumbsup from reactions where user_email='".$profile2['user_email']."' and thumbsup=1");
+	
+	if($insert_posts1){	
+											// Return the number of rows in result set
+							$rowcount1=mysqli_num_rows($insert_posts1);
+						    echo $rowcount1;
+	
+	
+					  }
+
+			else{?>0<?php }?></span></p>	
+	<br>
+	
+	<p><h2 style="font-family: 'Salsa'">Number of Negative Reactions:</h2><span style="font-family: 'Patua One'; font-size:2em; font-weight:bold"><?php 
+	
+	$insert_posts2 = mysqli_query($conn,"select  thumbsdown from reactions where user_email='".$profile2['user_email']."' and thumbsdown=-1");
+	
+	if($insert_posts2){	
+											// Return the number of rows in result set
+							$rowcount2=mysqli_num_rows($insert_posts2);
+						    echo $rowcount2;
+	
+	
+					  }
+
+			else{?>0<?php }?></span></p>	
+	<br>
+	
+	
+	
+	
 	<?php
 	
 	
-	}
-	
-	else{
-		echo '<script language="javascript">';
-        echo 'location.href="index.php"';
-        echo '</script>';
-		
-		
-		
 	}
 	
 }
@@ -94,6 +134,10 @@ if(isset($_POST['search_submit'])) {
 
 ?>
 
+
+<a href="index.php" class="btn btn-primary btn-lg" role="button">Back</a>
+	
+	<br>
 
 </center>
 
